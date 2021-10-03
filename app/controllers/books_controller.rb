f164ebdabe5c0936ff8882ede193bb92855280f6
yowasirasu <song1055v@gmail.com>
2021-10-03 08:24:53 +0000

    FF

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,9 +10,9 @@ class BooksController < ApplicationController
   end
 
   def create
-      book = Book.new(book_params)
-      if book.save
-        redirect_to books_path(book.id)
+      @book = Book.new(book_params)
+      if @book.save
+        redirect_to book_path(book.id)
       else
         render :new
       end
